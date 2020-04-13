9ec7f23a2b2cca5d412c21ac084dbc79b85d8726
zhangll6 <zhangll6@ziroom.com>
2020-04-13 09:32:59 +0800

    Create project

---
+++ src/test/java/com/example/zuul/demozuul/DemoZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuul.demozuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
